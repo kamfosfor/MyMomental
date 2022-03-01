1c663c3b575b49c4afc56691fd9b4ff8068dd0be
mmshooreshi <mmshooreshi@gmail.com>
2022-03-02 02:10:13 +0330

    meta

--- components/layout.jsx
+++ components/layout.jsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import styles from './layout.module.css'
+git import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 import Blob from '../components/blob'
@@ -15,7 +15,7 @@ export default function Layout({ children, home }) {
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
+          content="TEST version of Momental website landing page"
         />
         {/* <meta
           property="og:image"
